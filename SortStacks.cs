b0c1db3b7884bf30fd13e317a365c0501c3e089b
YAWAR MURTAZA KHUWAJA <yawarmurtaza@gmail.com>
2019-01-15 13:31:06 +0000

    Update SortStacks.cs

--- SortStacks.cs
+++ SortStacks.cs
@@ -1,10 +1,18 @@
+/// <summary>
+/// Sorts an unsorted stack and returns it.
+/// </summary>
+/// <param name="unsorted">Unsorted stack to be sorted.</param>
+/// <returns>Sorted stack.</returns>
 public Stack<int> SortStack(Stack<int> unsorted)
 {
+    if (unsorted == null) return null;
+    if (unsorted.Count < 2) return unsorted;
+
     Stack<int> partiallySorted = new Stack<int>(unsorted.Count);
-    int temp;
     bool sortDescending = true;
     while (!IsSorted(partiallySorted.ToArray()))
     {
+        int temp;
         if (sortDescending)
         {
             temp = unsorted.Pop();
@@ -50,11 +58,19 @@ public Stack<int> SortStack(Stack<int> unsorted)
     return partiallySorted;
 }
 
+/// <summary>
+/// Determines if given integer array is sorted (ascending or descending) 
+/// </summary>
+/// <param name="arr">Array to check</param>
+/// <returns>A value that indicates if the array is sorted. True is sorted, false otherwise.</returns>
 public  bool IsSorted(int[] arr)
 {
+    // edge cases...
     if (arr.Length < 1) return false;
+    if (arr.Length == 1) return true;
+    
     int diff = arr[1] - arr[0];
-
+    
     // if the diff is a positive number than its ascending order else descending
     if (diff > 0)
     {
@@ -78,6 +94,7 @@ public  bool IsSorted(int[] arr)
 
 
 // Unit tests...
+[TestCase(new int[] { 5 })]
 [TestCase(new int[] { -10, 55, 1, 4, -8, 2, 3, -5, 50, -200, 33, 7, 9, -3 })]
 [TestCase(new int[] { 10, 55, 1, 4, 8, 2, 3, 5, 50 })]
 [TestCase(new int[] { 4, 8, 2, 3, 5, 50 })]
@@ -87,6 +104,36 @@ public void SortStack(int[] unsortedArrayToStack)
     Stack<int> sorted = runner.SortStack(unSorted);
 
     // Assert..
+
     Assert.IsTrue(runner.IsSorted(sorted.ToArray()));
+}
+
+[Test]
+public void SortStack_ShouldReturnNullWhenInputStackIsNull()
+{
+    Stack<int> result = runner.SortStack(null);
+    Assert.IsNull(result);
+}
+
+[Test]
+public void SortStack_ShouldReturnEmptyStackWhenAnEmpryStackIsInput()
+{
+    Stack<int> unSorted = new Stack<int>(new int[] { });
+    Stack<int> sorted = runner.SortStack(unSorted);
+
+    // Assert... since empty stack is not considered as sorted....
+    Assert.IsFalse(runner.IsSorted(sorted.ToArray()));
+}
+
+
+/*IsSorted unit tests...*/
+[TestCase(new int[] { 5 })]
+[TestCase(new int[] { 1, 2, -3, 4, 5, -6, 7, 8, -9 }, false)]
+[TestCase(new int[] { 10, 2, -3, -4, -5, -6, -7, -8, -9 }, true)]
+[TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, true)]
+public void IsSorted(int[] arr, bool expectedResult)
+{
+    bool result = runner.IsSorted(arr);
+    Assert.AreEqual(expectedResult, result);
 }
  
