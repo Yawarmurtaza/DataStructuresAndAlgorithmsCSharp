3c167328c1428899a1776bf03ca5aada1ce415a5
YAWAR MURTAZA KHUWAJA <yawarmurtaza@gmail.com>
2019-01-17 20:58:55 +0000

    Update FindLongestNonRecurringString.cs

--- FindLongestNonRecurringString.cs
+++ FindLongestNonRecurringString.cs
@@ -1,3 +1,10 @@
+private struct VisitedCharIndex
+{
+    public int Index { get; set; }
+    public bool Visited { get; set; }
+    public char Char { get; set; }  
+}
+
 public int[] FindLongestNonRecurringString(string input)
 {
     if (input == null) return null;
@@ -10,49 +17,60 @@ public int[] FindLongestNonRecurringString(string input)
         return new int[] { 0, 0, 1 };
     }
 
-    int[] visited = new int[255]; // get the unicode....
+    VisitedCharIndex[] visited = new VisitedCharIndex[255];
+    int startIndex = 0;
     int tempLen = 0;
     int finalLen = 0;
-    int endIndex = 0;
+    int lenStartIndex = 0;
     int[] result = new int[3];
     for (int i = 0; i < input.Length; i++)
     {
         char c = input[i];
-        visited[c]++;
-        tempLen++;
-        if (visited[c] > 1)
+        if (visited[c].Visited)
         {
-            tempLen = 1; // because new string will start from this count...
+            if (startIndex < visited[c].Index + 1)
+                startIndex = visited[c].Index + 1;
 
+            tempLen = i - startIndex + 1;
+            visited[c].Index = i; // last seen index..
         }
+        else
+        {
+            visited[c].Index = i;
+            visited[c].Visited = true;
+            visited[c].Char = c;
+            tempLen++;
+        }
+
         if (finalLen < tempLen)
         {
-            endIndex = i;
+            lenStartIndex = startIndex; // we need to store the start index of longest length.
             finalLen = tempLen;
         }
-
-        int startIndex = endIndex - finalLen + 1;
-        result[0] = startIndex;
-        result[1] = endIndex;
-        result[2] = finalLen;
     }
-
+    
+    result[0] = lenStartIndex;
+    result[1] = lenStartIndex + finalLen - 1;
+    result[2] = finalLen;
     return result;
 }
 
-// unit tests..
+// Unit tests
 [TestCase("1234567890_=-()*&^%$£!", 0, 21, 22)]
-[TestCase("1234567890", 0, 9, 10)]
-[TestCase("qqqqqqqwwwwwrrrrwwwwtteteyrtrytuyiuoupupuoyoiyiruryurytuturirueyasdfghjklmnbvcxz", 63, 79, 17)]
+[TestCase("qqqqqqqwwwwwrrrrwwwwtteteyrtrytuyiuoupupuoyoiyiruryurytuturirueyasdfghjklmnbvcxz", 59, 79, 21)]
 [TestCase("asdfghjklmnbvcxzqqqqqqqwwwwwrrrrwwwwtteteyrtrytuyiuoupupuoyoiyiruryurytuturiruey", 0, 16, 17)]
 [TestCase("", 0, 0, 0)]
+[TestCase("1234567890", 0, 9, 10)]
 [TestCase("a", 0, 0, 1)]
 [TestCase("abcadefghhijklmnopqqqqqqqqqqq", 9, 18, 10)]
 [TestCase("abcadefghhijklmnopq", 9, 18, 10)]
-[TestCase("abcadefgh", 3, 8, 6)]
+[TestCase("abcadefgh", 1, 8, 8)]
+[TestCase("MyNameIsImanbcdfghjklggggggggggg", 7, 20, 14)]
+[TestCase("MyNameIsImanbcdfg", 7, 16, 10)]
+[TestCase("MyNameIsIman", 1, 7, 7)]
 public void FindLongestNonRecurringString(string input, int startIndex, int endIndex, int expectedLen)
 {
-    int[] result = runner.FindLongestNonRecurringString(input);
+    int[] result = runner.FindLongestNonRecurringString(input.ToLower());
     Assert.AreEqual(expectedLen, result[2]);
     Assert.AreEqual(endIndex, result[1]);
     Assert.AreEqual(startIndex, result[0]);
